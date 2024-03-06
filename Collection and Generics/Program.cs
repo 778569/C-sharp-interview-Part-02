f0b7b208c8b3a8c48d54ea2225a666722cbfb9d2
Kavinda Bandara <kavindabandara94@gmail.com>
2024-03-06 12:05:22 +0530

    Array For each

--- Collection and Generics/Program.cs
+++ Collection and Generics/Program.cs
@@ -2,6 +2,31 @@
 {
     private static void Main(string[] args)
     {
-        
+        int[] nums;
+        double[] otherNums = new double[5];
+
+        string[] availablePets = { "cat", "dog", "fish" };
+        string[] unavailablePets = { "bird", "rabbit", "hamster", "gerbil" };
+
+        for (int i = 0; i < availablePets.Length; i++)
+        {
+            Console.WriteLine(availablePets[i]);
+        }
+
+        Console.WriteLine();
+
+        foreach (var x in unavailablePets)
+        {
+            Console.WriteLine(x);
+        }
+
+        Console.WriteLine();
+
+        Array.ForEach(availablePets, Console.WriteLine);
+        Console.WriteLine();
+
+        Array.ForEach(unavailablePets, Console.WriteLine);
+
+        Console.ReadLine();
     }
 }
