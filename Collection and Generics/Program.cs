442079e81166bf5ee3d1910f92aea01583856c22
Kavinda Bandara <kavindabandara94@gmail.com>
2024-03-06 12:14:50 +0530

    Change the object each other

--- Collection and Generics/Program.cs
+++ Collection and Generics/Program.cs
@@ -8,19 +8,37 @@
         string[] availablePets = { "cat", "dog", "fish" };
         string[] unavailablePets = { "bird", "rabbit", "hamster", "gerbil" };
 
-        for (int i = 0; i < availablePets.Length; i++)
-        {
-            Console.WriteLine(availablePets[i]);
-        }
 
-        Console.WriteLine();
 
-        foreach (var x in unavailablePets)
-        {
-            Console.WriteLine(x);
-        }
+        var fishIndex = 2;
+        var birdIndex = 0;
 
-        Console.WriteLine();
+
+
+        string pet1 = availablePets[fishIndex];
+
+        string pet2 = unavailablePets[birdIndex];
+
+        availablePets[fishIndex] = pet2;
+
+        unavailablePets[birdIndex]= pet1;
+
+
+
+
+        //for (int i = 0; i < availablePets.Length; i++)
+        //{
+        //    Console.WriteLine(availablePets[i]);
+        //}
+
+        //Console.WriteLine();
+
+        //foreach (var x in unavailablePets)
+        //{
+        //    Console.WriteLine(x);
+        //}
+
+        //Console.WriteLine();
 
         Array.ForEach(availablePets, Console.WriteLine);
         Console.WriteLine();
